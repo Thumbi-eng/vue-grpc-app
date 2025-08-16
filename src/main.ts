93b0bc1f6e0edb5b96c2ae0d2a69c3d088091114
Mustang <victor@techbridgeinnovation.co>
2025-08-16 14:20:02 +0300

    learning vue3 with vuetify this is a todolist

---
+++ src/main.ts
@@ -0,0 +1,38 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+// the vuetify ImportsNotUsedAsValues
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// the material design icons
+
+
+import '@mdi/font/css/materialdesignicons.css'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
+// Vuetify instance
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: { mdi },
+  },
+})
+
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(vuetify)
+app.mount('#app')
